68be887d672860d8c37cf9aed0275dc0ab3f71d3
Andres Olave <Andres Olave>
2023-08-02 12:57:38 +1000

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMjc3MQ/spectral.js?branch_slug=master&token=08f11f1f-1553-4265-a215-721078000429";
+export default { extends: ruleset };
